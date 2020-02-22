b66570194c2d62db833ad0f0ed132901a5986f62
Mikolaj Figurski <meek.f80@gmail.com>
2020-02-22 14:38:16 -0500

    Init newsclient

---
+++ src/com/company/NewsClient.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+public class NewsClient {
+
+    private static final String API_KEY = "66c2e12a4d234e33b3df8d3057ea4a5a";
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String ENDPOINT = "http://newsapi.org/v2";
+
+    public static void main(String[] args) {
+
+    }
+
+}
