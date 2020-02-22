219f037271f5bd5aad6d8c5ec5624ced811a950f
Mikolaj Figurski <meek.f80@gmail.com>
2020-02-22 14:59:45 -0500

    request expansion

--- src/com/company/NewsClient.java
+++ src/com/company/NewsClient.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 
@@ -18,6 +19,36 @@ public class NewsClient {
     public static void getByKeyword(String keyword) {
         String url = ENDPOINT + "?from=2020-01-22&sortBy=popularity&apiKey=" + API_KEY + "&q=" + keyword;
         System.out.println(url);
+    }
+
+    private static String sendGet(String url) {
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", USER_AGENT);
+            int responseCode = con.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) { // success
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                return response.toString();
+            } else {
+                System.out.println("HTTP STATUS: " + responseCode);
+            }
+        } catch(Exception e) {
+            // nobody cares, stfu
+            return "AHHHH";
+        }
+        return "";
     }
 
 }
