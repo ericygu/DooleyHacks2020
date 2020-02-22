7da9a2bd66fdff818e39214778ff321f787bfb51
ericygu <gudmaneric@gmail.com>
2020-02-22 16:39:37 -0500

    str processor + count

---
+++ valueKeywords.py
@@ -0,0 +1,14 @@
+#think about how keywords are set to values
+def word_count(str):
+    counts = dict()
+    words = str.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
+
+print(word_count('poop poop poop shit crap'))
