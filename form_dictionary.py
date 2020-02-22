1f451c9f5148f5e3ceebca46d37aa62058a03bfb
Mikolaj Figurski <meek.f80@gmail.com>
2020-02-22 17:00:12 -0500

    Refactor form dictionary

--- form_dictionary.py
+++ form_dictionary.py
@@ -1,27 +1,21 @@
+import json
+
 dictionary = {}
 
 
 # think about how keywords are set to values
 # format {'potato': 4, 'oil': 1}
-
-
 def insert_dictionary(str):
-    counts = dict()
     words = str.split()
-
     for word in words:
         if word in counts:
             dictionary[word] += 1
         else:
             dictionary[word] = 1
 
-    return counts
-
 
 insert_dictionary("potato potato potato potato oil")
 print(dictionary)
-
-print(word_count("potato potato potato potato oil"))
 
 
 def write_dictionary():
@@ -32,3 +26,7 @@ def write_dictionary():
 def read_dictionary():
     with open('dictionary.json') as f:
         return json.load(f)
+
+
+if __name__ == '__main__':
+    pass
